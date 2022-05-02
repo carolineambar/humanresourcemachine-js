0d79a7849fd4765adcadd9dbca9f50bd36bd2911
caroline <ambar.caroline@gmail.com>
2022-05-02 09:47:05 -0300

    Inclua anos 1, 4 e 6

---
+++ scripts/year6.js
@@ -0,0 +1,15 @@
+function year6(items) {
+    counter = 0
+    const newItems = []
+    while(counter < items.length) {
+        const soma = items[counter] + items[counter+1]
+        counter = counter+2
+        newItems.push(soma)
+    }
+    return newItems
+
+}
+
+const inbox = [5, 8, 0, -4, 0, 3]
+
+console.log(year6(inbox))
