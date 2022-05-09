58cbb6ca42b361d03a4ec3811eedede48ae18f1a
caroline <ambar.caroline@gmail.com>
2022-05-09 08:33:17 -0300

    Inclua exercícios 13 e 14

---
+++ scripts/year14.js
@@ -0,0 +1,19 @@
+function year14(items) {
+    let counter = 0
+    const newItems = []
+    while(counter < items.length) {
+        if(items[counter] >= items[counter + 1]){
+            newItems.push(items[counter])
+        }
+        else if(items[counter + 1] >= items[counter]){
+            newItems.push(items[counter + 1])
+        }
+        counter = counter+2
+    }
+    
+    return newItems
+}
+
+const inbox = [6, 1, -3, -8, 3, 3, 7, 6]
+
+console.log(year14(inbox))
